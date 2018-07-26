e088e9945a0f1cf034fa5b1cf78213887079a2e5
devtuananh <tran.tuan.anhb@framgia.com>
2018-07-26 08:55:53 +0700

    Create fake data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :requests, dependent: :destroy
   has_many :ratings, dependent: :destroy
+  has_secure_password
 
-  validates :name, :phone, :email, presence: true
+  validates :name, presence: true, length: {maximum: Settings.validates.name_maximum}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true,
+    length: {maximum: Settings.validates.email_maximum}, format: {with: VALID_EMAIL_REGEX}
+  validates :password, presence: true, length:
+    {minimum: Settings.validates.password_minimum}, allow_nil: true
+  validates :phone, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create! name: "Admin", email: "admin@gmail.com", phone: "0243 585 2568",
+  role: 2, password: "123123", password_confirmation: "123123"
+
+User.create! name: "Staff", email: "staff@gmail.com", phone: "0243 256 2548",
+  role: 1, password: "123123", password_confirmation: "123123"
+
+50.times do
+  User.create! name: Faker::Name.unique.name,
+               email: Faker::Internet.unique.email,
+               phone: Faker::PhoneNumber.phone_number,
+               password: "123123",
+               password_confirmation: "123123"
+end
+
+users = User.order(:created_at).take 6
+10.times do
+  start_time = Faker::Time.between(Time.zone.now - 3, Time.zone.now)
+  end_time = Time.zone.now
+  status = "1"
+  users.each {|user| user.bookings.create! start_time: start_time,
+    end_time: end_time}
+end
+
+10.times do
+  content = Faker::Lorem.sentence
+  status = "1"
+  priority = Faker::Number.number 1
+  users.each {|user| user.requests.create! content: content, status: status,
+    priority: priority}
+end
+
+10.times do
+  content = Faker::Lorem.sentence
+  users.each {|user| user.ratings.create! content: content}
+end
+
+10.times do
+  Category.create! name: Faker::Lorem.characters(5),
+                   price: Faker::Number.number 6,
+                   description: Faker::Lorem.paragraph(5)
+end
+
+categories = Category.order(:created_at).take 6
+20.times do
+  label = Faker::Lorem.characters(5)
+  floor = Faker::Number.number 2
+  status = Faker::Number.number 1
+  categories.each {|category| category.rooms.create! label: label, floor: floor,
+    status: status}
+end
