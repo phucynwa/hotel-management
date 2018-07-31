5ccd72db45be67c1ad0e5e85aaac7b9f01de6361
devtuananh <tran.tuan.anhb@framgia.com>
2018-07-31 10:24:50 +0700

    Show, Edit, Delete Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,12 @@
 class CategoriesController < ApplicationController
   before_action :logged_in_user, :vetify_admin
+  before_action :load_category, except: %i(index new create)
+
+  def index
+    @categories = Category.by_latest.page(params[:page]).per Settings.show
+  end
+
+  def show; end
 
   def new
     @category = Category.new
@@ -16,7 +23,35 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t ".category_update"
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:success] = t ".success"
+      redirect_to categories_path
+    else
+      flash[:danger] = t ".not_found"
+      redirect_to categories_path
+    end
+  end
+
   private
+
+  def load_category
+    @category = Category.find_by id: params[:id]
+    return if @category
+    flash[:danger] = t ".not_found", id: params[:id]
+    redirect_to categories_path
+  end
 
   def category_params
     params.require(:category).permit :name, :price, :description
